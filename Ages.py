2e18989d2e946a324ab78263dd45c5aa5cbc8c9c
noamgl19-meet <noamgl19@meet.mit.edu>
2017-07-25 15:49:18 +0200

    Stopping here

---
+++ Ages.py
@@ -0,0 +1,21 @@
+#asking for the user to put an input of his age
+print("enter your age")
+#installing first age variuble
+age1 = input()
+#asking for second age
+print("enter second age")
+#installing second age variuble
+age2 = input()
+
+
+
+
+
+
+def average(x,y):
+    average = (int(x) + int(y)) / 2
+    return average
+
+avg = average(age1, age2)
+print("the average is " + str(avg))
+
---
+++ input.py
@@ -0,0 +1,5 @@
+print("what is your name?")
+name = input()
+print("how old are you?")
+age = input()
+print("hi " + str(name) + " you are " +str(age) +" years old")
